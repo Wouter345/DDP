562fa31cfd3d1640baf16d7b36377f165f775ccc
Wouter Globevnik <r0792566@stud-r8-19.esat.kuleuven.be>
2023-10-13 00:19:31 +0200

    Implemented HW257
    Implemented an adder which calculates 257 bits at a time, same design as for HW200 but 2 fewer clock cycles needed. Slack of 0.5ns. HW257 is fully functioning.

--- HW257/hw_project/src/rtl/adder/adder.v
+++ HW257/hw_project/src/rtl/adder/adder.v
@@ -11,11 +11,13 @@ module mpadder(
   output wire          done   
   );
 
+    
+
 // Task 1
     // Describe a 1027-bit register for A
     // It will save the input data when enable signal is high
 
-    reg          regA_en;
+    reg           regA_en;
     wire [1026:0] regA_in;
     reg  [1026:0] regA_out;
     always @(posedge clk)
@@ -27,7 +29,7 @@ module mpadder(
 // Task 2
     // Describe a 1027-bit register for B
 
-    reg          regB_en;
+    reg           regB_en;
     wire [1026:0] regB_in;
     reg  [1026:0] regB_out;
     always @(posedge clk)
@@ -43,19 +45,22 @@ module mpadder(
     //   - the output of regA shifted-right by 200
     // Also connect the output of Mux to regA's input
     
-    reg          muxA_sel;
+    reg           muxA_sel;
     wire [1026:0] muxA_Out;
     assign muxA_Out = (muxA_sel == 0) ? in_a : {257'b0,regA_out[1026:257]};
-    
+
     assign regA_in = muxA_Out;
    
    
-// Task 4
+// Task 4   
+    // Describe a 2 input mux choosing B or ~B+1
+    wire [1026:0] MuxBin_Out;
+    assign MuxBin_Out = (subtract == 0)? in_b : ~in_b;
+    
     // Describe a 2-input 1027-bit Multiplexer for B
-
-    reg          muxB_sel;
+    reg           muxB_sel;
     wire [1026:0] muxB_Out;
-    assign muxB_Out = (muxB_sel == 0) ? in_b : {257'b0,regB_out[1026:257]};
+    assign muxB_Out = (muxB_sel == 0) ? MuxBin_Out : {257'b0,regB_out[1026:257]};
     
     assign regB_in = muxB_Out;
     
@@ -67,23 +72,21 @@ module mpadder(
 
     wire [256:0] operandA;
     wire [256:0] operandB;
-    wire        carry_in;
+    wire         carry_in;
     wire [256:0] Sum;
-    wire        carry_out;
-    
+    wire         carry_out;
 
-    assign {carry_out,Sum} = (subtract==0)? operandA + operandB + carry_in: operandA - operandB - carry_in;
-    
+    assign {carry_out,Sum} = operandA + operandB + carry_in;
 
 // Task 6
-    // Describe a 1028-bit register for storing the sum
+    // Describe a 1200-bit register for storing the sum
 
-    reg          regSum_en;
+    reg           regSum_en;
     reg  [1027:0] regSum;
     always @(posedge clk)
     begin
-        if(~resetn)             regSum <= 1027'b0;
-        else if (regSum_en)  regSum <= {Sum, regSum[1026:257]};
+        if(~resetn)          regSum <= 1027'b0;
+        else if (regSum_en)  regSum <= {Sum, regSum[1027:257]};
     end
 
 // Task 7
@@ -103,12 +106,16 @@ module mpadder(
     //   - 0
     //   - carry-out
     
+    reg muxsub_sel;
+    wire muxsub_in;
+    assign muxsub_in = (muxsub_sel == 0) ? regCout : subtract;
+    
     reg  muxCarryIn_sel;
     wire muxCarryIn;
-
-    assign muxCarryIn = (muxCarryIn_sel == 0) ? 1'b0 : regCout;
-
-
+    assign muxCarryIn = (muxCarryIn_sel == 0) ? 1'b0 : muxsub_in;
+    
+    //assign muxCarryIn = (muxCarryIn_sel == 0)? 1'b0 : ((muxsub_sel == 0) ? regCout : subtract);
+    
 // Task 9
     // Connect the inputs of adder to the outputs of A and B registers
     // and to the carry mux
@@ -118,8 +125,11 @@ module mpadder(
     assign carry_in = muxCarryIn;
     
 // Task 10
-    // Describe output, concatenate the registers of carry_out and result
-    assign result = regSum;
+    // Describe output, previous 5 calculations from regSum and last calculation
+    wire      carry;
+    
+    assign carry = subtract ^ regSum[1027];
+    assign result = {carry,regSum[1026:0]};
 
 // Task 11
     // Describe state machine registers
@@ -150,6 +160,7 @@ module mpadder(
                 muxA_sel       <= 1'b0;
                 muxB_sel       <= 1'b0;
                 muxCarryIn_sel <= 1'b0;
+                muxsub_sel     <= 1'b0;
             end
 
             // Enable registers, switch muxsel, no carryin
@@ -160,12 +171,13 @@ module mpadder(
                                 regSum_en      <= 1'b1;
                                 regCout_en     <= 1'b1;
                                 muxA_sel       <= 1'b1;
-                                muxB_sel       <= 1'b1;
-                                muxCarryIn_sel <= 1'b0;
+                                muxB_sel       <= 1'b1;  
+                                muxCarryIn_sel <= 1'b1;
+                                muxsub_sel     <= 1'b1;
             end
 
             // Calculate the second addition
-            3'd2,3'd3,3'd4,3'd5: begin
+            3'd2,3'd3,3'd4: begin
                                 regA_en        <= 1'b1;
                                 regB_en        <= 1'b1;
                                 regSum_en      <= 1'b1;
@@ -173,16 +185,18 @@ module mpadder(
                                 muxA_sel       <= 1'b1;
                                 muxB_sel       <= 1'b1;
                                 muxCarryIn_sel <= 1'b1;
+                                muxsub_sel     <= 1'b0;
             end
 
             default: begin
-                regA_en        <= 1'b0;
-                regB_en        <= 1'b0;
-                regSum_en      <= 1'b1;
-                regCout_en     <= 1'b0;
-                muxA_sel       <= 1'b0;
-                muxB_sel       <= 1'b0;
-                muxCarryIn_sel <= 1'b0;
+                                regA_en        <= 1'b0;
+                                regB_en        <= 1'b0;
+                                regSum_en      <= 1'b1;
+                                regCout_en     <= 1'b0;
+                                muxA_sel       <= 1'b0;
+                                muxB_sel       <= 1'b0;
+                                muxCarryIn_sel <= 1'b0;
+                                muxsub_sel     <= 1'b0;
             end
 
         endcase
@@ -200,12 +214,12 @@ module mpadder(
                 else
                     nextstate <= 3'd0;
                 end
-                
-            3'd1,3'd2,3'd3,3'd4 : nextstate <= state+1;
+            3'd1 : nextstate <= 3'd2;    
+            3'd2 : nextstate <= 3'd3;
+            3'd3 : nextstate <= 3'd4;
+            3'd4 : nextstate <= 3'd0;
             
-            3'd5 : nextstate <= 2'd0;
-            
-            default: nextstate <= 2'd0;
+            default: nextstate <= 3'd0;
         endcase
     end
 
